431fa47d204baab9f1577336f8c9fd699908a3fb
wuzhiming <wuzhiming.chukong@gmail.com>
2018-04-25 15:23:03 +0800

    change comment to matches the function definition

--- cocos/2d/CCRenderTexture.h
+++ cocos/2d/CCRenderTexture.h
@@ -175,17 +175,17 @@ public:
 
     /** saves the texture into a file. The format could be JPG or PNG. The file will be saved in the Documents folder.
         Returns true if the operation is successful.
-     * Notes: since v3.x, saveToFile will generate a custom command, which will be called in the following render->render().
+     * Notes: since v3.x, saveToPath will generate a custom command, which will be called in the following render->render().
      * So if this function is called in a event handler, the actual save file will be called in the next frame. If we switch to a different scene, the game will crash.
      * To solve this, add Director::getInstance()->getRenderer()->render(); after this function.
      *
-     * @param path The path.
+     * @param path The file path.
      * @param format The image format.
      * @param isRGBA The file is RGBA or not.
      * @param callback When the file is save finished,it will callback this function.
      * @return Returns true if the operation is successful.
      */
-    bool saveToPath(const std::string& filename, Image::Format format, bool isRGBA = true, std::function<void (RenderTexture*, const std::string&)> callback = nullptr);
+    bool saveToPath(const std::string& path, Image::Format format, bool isRGBA = true, std::function<void (RenderTexture*, const std::string&)> callback = nullptr);
 
     /** Listen "come to background" message, and save render texture.
      * It only has effect on Android.
