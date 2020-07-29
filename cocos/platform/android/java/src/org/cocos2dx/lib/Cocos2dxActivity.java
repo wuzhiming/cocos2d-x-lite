fa7a26364d447d1fac9711d2fe225a3af399e8f6
wuzhiming <wuzhiming.chukong@gmail.com>
2020-07-29 11:52:31 +0800

    add SYSTEM_UI_FLAG_IMMERSIVE_STICKY flag
    Signed-off-by: wuzhiming <wuzhiming.chukong@gmail.com>

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -285,6 +285,7 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 
+            flag |= View.class.getDeclaredField("SYSTEM_UI_FLAG_IMMERSIVE_STICKY").getInt(null);
             View view = getWindow().getDecorView();
             view.setSystemUiVisibility(flag);
 
