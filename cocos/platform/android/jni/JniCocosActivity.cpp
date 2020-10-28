ad65e41c5d58f67cbfd6b457be5b5694bbebc231
minggo <nudtzxm@gmail.com>
2020-10-28 11:34:18 +0800

    simplify codes

--- cocos/platform/android/jni/JniCocosActivity.cpp
+++ cocos/platform/android/jni/JniCocosActivity.cpp
@@ -1,15 +1,28 @@
 #include "JniCocosActivity.h"
 #include "platform/android/jni/JniHelper.h"
 #include "platform/android/FileUtils-android.h"
+#include "platform/Application.h"
 #include <jni.h>
 #include <android/asset_manager_jni.h>
 #include <android/native_window_jni.h>
 #include <thread>
 
-void cocos_main(ANativeWindow *) __attribute__((weak));
+cc::Application *cocos_main(int, int) __attribute__((weak));
 
 namespace cc {
 CocosApp cocosApp;
+
+void glThreadEntry(ANativeWindow *window) {
+	int width = ANativeWindow_getWidth(window);
+	int height = ANativeWindow_getHeight(window);
+	auto game = cocos_main(width, height);
+	if (!game) return;
+
+	while (1) {
+		JniHelper::callStaticVoidMethod("org.cocos2dx.lib.Cocos2dxHelper", "flushTasksOnGameThread");
+		game->tick();
+	}
+}
 }
 
 extern "C" {
@@ -26,7 +39,7 @@ JNIEXPORT void JNICALL Java_org_cocos2dx_lib_CocosActivity_onCreateNative(JNIEnv
 JNIEXPORT void JNICALL Java_org_cocos2dx_lib_CocosActivity_onSurfaceCreatedNative(JNIEnv *env, jobject obj, jobject surface) {
     cc::cocosApp.window = ANativeWindow_fromSurface(env, surface);
 
-    std::thread glThread(cocos_main, cc::cocosApp.window);
+    std::thread glThread(cc::glThreadEntry, cc::cocosApp.window);
     glThread.detach();
 }
 
