ac2a8d9f11a892439f982d18242ed7d89a2ac87c
wuzhiming <wuzhiming.chukong@gmail.com>
2020-11-03 21:29:05 +0800

    first version of touch handler

--- cocos/platform/android/java/src/org/cocos2dx/lib/CocosActivity.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/CocosActivity.java
@@ -7,6 +7,7 @@ import android.content.res.AssetManager;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -18,20 +19,32 @@ import java.io.File;
 
 public class CocosActivity extends Activity implements SurfaceHolder.Callback {
     private native void onCreateNative(Activity activity, AssetManager assetManager, String obbPath, int sdkVersion);
+
     private native void onSurfaceCreatedNative(Surface surface);
+
     private native void onSurfaceChangedNative(int width, int height);
+
     private native void onSurfaceDestroyNative();
+
     private native void onPauseNative();
+
     private native void onResumeNative();
+
     private native void onStopNative();
+
     private native void onStartNative();
+
     private native void onLowMemoryNative();
+
     private native void onWindowFocusChangedNative(boolean hasFocus);
 
     private boolean mDestroyed;
     private SurfaceHolder mSurfaceHolder;
     private FrameLayout mFrameLayout;
     private SurfaceView mSurfaceView;
+    private boolean engineInit = false;
+
+    private CocosTouchHandler mTouchHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +58,7 @@ public class CocosActivity extends Activity implements SurfaceHolder.Callback {
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
         initView();
         onCreateNative(this, getAssets(), getAbsolutePath(getObbDir()), Build.VERSION.SDK_INT);
+        mTouchHandler = new CocosTouchHandler(this);
     }
 
     private static String getAbsolutePath(File file) {
@@ -64,54 +78,62 @@ public class CocosActivity extends Activity implements SurfaceHolder.Callback {
         mFrameLayout.addView(mSurfaceView);
     }
 
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!engineInit) return false;
+        //handle touch event
+        return mTouchHandler.onTouchEvent(event);
+    }
+
     @Override
     protected void onDestroy() {
         mDestroyed = true;
-//        if (mSurfaceHolder != null) {
-//            onSurfaceDestroyNative();
-//            mSurfaceHolder = null;
-//        }
+        if (mSurfaceHolder != null) {
+            onSurfaceDestroyNative();
+            mSurfaceHolder = null;
+        }
         super.onDestroy();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-//        onPauseNative();
+        onPauseNative();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-//        onResumeNative();
+        onResumeNative();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-//        onStopNative();
+        onStopNative();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-//        onStartNative();
+        onStartNative();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-//        if (!mDestroyed) {
-//            onLowMemoryNative();
-//        }
+        if (!mDestroyed) {
+            onLowMemoryNative();
+        }
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
-//        super.onWindowFocusChanged(hasFocus);
-//        if (!mDestroyed) {
-//            onWindowFocusChangedNative(hasFocus);
-//        }
+        super.onWindowFocusChanged(hasFocus);
+        if (!mDestroyed) {
+            onWindowFocusChangedNative(hasFocus);
+        }
     }
 
     @Override
@@ -119,23 +141,26 @@ public class CocosActivity extends Activity implements SurfaceHolder.Callback {
         if (!mDestroyed) {
             mSurfaceHolder = holder;
             onSurfaceCreatedNative(holder.getSurface());
+
+            engineInit = true;
         }
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-//        if (!mDestroyed) {
-//            mSurfaceHolder = holder;
-//            onSurfaceChangedNative(width, height);
-//        }
+        if (!mDestroyed) {
+            mSurfaceHolder = holder;
+            onSurfaceChangedNative(width, height);
+        }
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         mSurfaceHolder = null;
-//        if (!mDestroyed) {
-//            onSurfaceDestroyNative();
-//        }
+        if (!mDestroyed) {
+            onSurfaceDestroyNative();
+            engineInit = false;
+        }
     }
 
     private void onLoadNativeLibraries() {
